78e3da42a7c9ea0ae598996b4a8369d87658a94b
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-01 16:12:01 -0500

    Push to GitHub

---
+++ README.md
@@ -0,0 +1,13 @@
+# Happy Valentines Day, Sam!
+
+### To install:
+Run the following in your command prompt:
+``npm i -g for-sam-2019``
+
+### To run:
+Run the following in your command prompt:
+``happy-valentines-day``
+
+And go to **localhost:4000** in your browser.
+
+*<3*
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+#! /usr/bin/env node
 const express = require('express')
+const path = require('path')
 const comments = require('./comments')
 const pictures = require('./pictures')
 const app = express()
 
 app.set('view engine','hbs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(4000, () => {
 })
 
 app.get("/", (req,res) => {
-    res.render("index")
+    res.render(path.join(__dirname, "/views/index"))
 })
 
 app.get("/pictures", (req,res) => {
     const pictureIndex = Math.floor(Math.random() * pictures.length)
     const commentIndex = Math.floor(Math.random() * comments.length)
-    res.render("pictures", {pictureFile: pictures[pictureIndex], comment: comments[commentIndex]})
+    res.render(path.join(__dirname,'views/pictures'), {pictureFile: pictures[pictureIndex], comment: comments[commentIndex]})
+})
+
+app.get("/public/style.css", (req,res) => {
+    res.send("hello world!")
 })
